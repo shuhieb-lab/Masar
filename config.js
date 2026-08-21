94256164c9d2a561769743f84f5eaa013a2eaab7
shuhieb-lab <shuhieb@gmail.com>
2026-08-21 11:21:49 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MASAR_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://ifxprsxcwdwwywqlyhfk.supabase.co',
+  supabaseAnonKey: 'sb_publishable_I7aCBtpCaCwIAtuVt4ursw_Us_gTOfQ'
 };
